e5bd7a8110cd2ca779902d64a2c64f9abf0da1cf
changdy <117969632@qq.com>
2017-03-10 08:10:54 +0800

    日期增加格式化

--- smzdm-spider/src/main/java/com/smzdm/util/DateTimeHandler.java
+++ smzdm-spider/src/main/java/com/smzdm/util/DateTimeHandler.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by Changdy on 2017/3/6.
  */
 @Service
 public class DateTimeHandler {
+    private DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+
     public LocalDateTime convertToDate(String string) {
         LocalDateTime localDateTime = LocalDateTime.now();
         try {
@@ -20,7 +24,7 @@ public class DateTimeHandler {
                     LocalDate today = LocalDate.now();
                     int year = today.getYear();
                     string = year + "-" + string;
-                    localDateTime = LocalDateTime.parse(string);
+                    localDateTime = LocalDateTime.parse(string,dateTimeFormatter);//略不爽，我还以为java8已经默认有日期自动转换了
                 } else {
                     LocalDate localDate = LocalDate.parse(string);
                     localDateTime = localDate.atTime(20, 0);
