3e21cf3598577e7314e748a2890f66a257654bec
changdy <117969632@qq.com>
2017-03-12 19:47:15 +0800

    1,fastjson依赖 2,spring mvc乱码解决 3,table初次尝试

---
+++ smzdm-model/src/main/java/com/smzdm/model/CommodityParams.java
@@ -0,0 +1,52 @@
+package com.smzdm.model;
+
+/**
+ * Created by Changdy on 2017/3/12.
+ */
+public class CommodityParams {
+    private String sort;
+    private String order;
+    private Long offset;
+    private Long limit;
+    private String title;
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        this.order = order;
+    }
+
+    public Long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(Long offset) {
+        this.offset = offset;
+    }
+
+    public Long getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Long limit) {
+        this.limit = limit;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- smzdm-view/src/main/java/com/smzdm/controller/TestController.java
+++ smzdm-view/src/main/java/com/smzdm/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.smzdm.controller;
-
+import com.alibaba.fastjson.JSON;
+import com.smzdm.mapper.CommodityMapper;
+import com.smzdm.model.Commodity;
+import com.smzdm.model.CommodityParams;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.*;
 
 /**
  * Created by Changdy on 2017/3/4.
@@ -10,9 +16,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
+    @Autowired
+    private CommodityMapper commodityMapper;
 
-    @RequestMapping("/start-spider")
-    public @ResponseBody String  startSpider() {
-        return "test";
+    @RequestMapping("/get-commodity")
+    public @ResponseBody String  getcommodity(CommodityParams CommodityParams) {
+        Commodity commodity = commodityMapper.selectByPrimaryKey(7014905l);
+        Map<String,Object> map  =new HashMap<>();
+        map.put("rows", new ArrayList(Collections.singletonList(commodity)));
+        map.put("total",1);
+        String s = JSON.toJSONString(map);
+        System.out.println(s);
+        return s;
     }
 }
