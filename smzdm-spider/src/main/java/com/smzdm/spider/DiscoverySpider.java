ea372fda13d1a8958b647db8b21002b01438bb0e
changdy <117969632@qq.com>
2017-03-19 10:27:32 +0800

    一次获取两条

--- smzdm-spider/src/main/java/com/smzdm/spider/DiscoverySpider.java
+++ smzdm-spider/src/main/java/com/smzdm/spider/DiscoverySpider.java
@@ -2,15 +2,20 @@ package com.smzdm.spider;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import org.springframework.stereotype.Service;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Changdy on 2017/3/17.
  */
+@Service
 public class DiscoverySpider implements PageProcessor {
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setUseGzip(true);
 
@@ -26,8 +31,11 @@ public class DiscoverySpider implements PageProcessor {
 
     public JSONArray getJSONArray() {
         Spider spider = Spider.create(new DiscoverySpider());
-        ResultItems resultItems = spider.get("http://faxian.smzdm.com/json_more?page=1");
+        List<ResultItems> resultItems = spider.getAll(Arrays.asList("http://faxian.smzdm.com/json_more?page=2", "http://faxian.smzdm.com/json_more?page=1"));
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(resultItems.get(1).get("json"));
+        jsonArray.add(resultItems.get(0).get("json"));
         spider.close();
-        return (JSONArray) resultItems.get("json");
+        return jsonArray;
     }
 }
