f11adea8eb993de3cfd550062dd97d99d730aa54
changdy <117969632@qq.com>
2017-03-27 20:00:28 +0800

    增加异常打印

--- smzdm-view/src/main/java/com/smzdm/service/impl/QueryCommodityServiceImpl.java
+++ smzdm-view/src/main/java/com/smzdm/service/impl/QueryCommodityServiceImpl.java
@@ -4,6 +4,8 @@ import com.smzdm.mapper.CommodityMapper;
 import com.smzdm.mapper.CommodityTimeInfoMapper;
 import com.smzdm.model.CommodityParams;
 import com.smzdm.service.QueryCommodityService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,8 @@ public class QueryCommodityServiceImpl implements QueryCommodityService {
 
     private final CommodityTimeInfoMapper commodityTimeInfoMapper;
 
+    private static Logger logger = LogManager.getLogger(QueryCommodityServiceImpl.class.getName());
+
     @Autowired
     public QueryCommodityServiceImpl(CommodityMapper commodityMapper, CommodityTimeInfoMapper commodityTimeInfoMapper) {
         this.commodityMapper = commodityMapper;
@@ -30,36 +34,37 @@ public class QueryCommodityServiceImpl implements QueryCommodityService {
     public Map<String, Object> queryCommodity(CommodityParams commodityParams) {
         List<Map<String, Object>> commodityList = commodityMapper.queryCommodity(commodityParams);
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("rows", commodityList);
-        if (commodityList != null) {
-            for (Map<String, Object> commodityMap : commodityList) {
-                Integer flag = (Integer) commodityMap.get("flag");
-                if (flag == null || flag == 0) {
-                    //非发现
-                    String comment = commodityTimeInfoMapper.getLastComment((Long) commodityMap.get("articleId"), false);
-                    if (comment.equals("0/0/0")) {
-                        commodityMap.put("comment", 0);
+        try {
+            resultMap.put("rows", commodityList);
+            if (commodityList != null) {
+                for (Map<String, Object> commodityMap : commodityList) {
+                    Integer flag = (Integer) commodityMap.get("flag");
+                    if (flag == null || flag == 0) {
+                        //非发现
+                        String comment = commodityTimeInfoMapper.getLastComment((Long) commodityMap.get("articleId"), false);
+                        if (comment != null) {
+                            if (comment.equals("0/0/0")) {
+                                commodityMap.put("comment", 0);
+                            } else {
+                                commodityMap.put("comment", comment);
+                            }
+                        }
                     } else {
-                        commodityMap.put("comment", comment);
-                    }
-                } else {
-                    //发现
-                    String comment = commodityTimeInfoMapper.getLastComment((Long) commodityMap.get("articleId"), true);
-                    if (comment.equals("0/-1/0") || comment.equals("0/0/0")) {
-                        commodityMap.put("comment", 0);
-                    } else {
-                        commodityMap.put("comment", comment);
+                        //发现
+                        String comment = commodityTimeInfoMapper.getLastComment((Long) commodityMap.get("articleId"), true);
+                        if (comment.equals("0/-1/0") || comment.equals("0/0/0")) {
+                            commodityMap.put("comment", 0);
+                        } else {
+                            commodityMap.put("comment", comment);
+                        }
                     }
                 }
-            }
-            int size = commodityList.size();
-            if (size == commodityParams.getLimit()) {
                 resultMap.put("total", commodityMapper.getCommodityCount(commodityParams));
             } else {
-                resultMap.put("total", size);
+                resultMap.put("total", 0);
             }
-        } else {
-            resultMap.put("total", 0);
+        } catch (Exception e) {
+            logger.error("ERROR", e);
         }
         return resultMap;
     }
