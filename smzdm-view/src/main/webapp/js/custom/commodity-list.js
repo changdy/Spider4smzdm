17328cc44e6fa8fb4a9e7c3de30d41d2c5754651
changdy <117969632@qq.com>
2017-05-21 23:31:36 +0800

    位置更改

---
+++ smzdm-view/src/main/webapp/js/custom/commodity-list.js
@@ -0,0 +1,109 @@
+/**
+ * Created by changdy on 2017/5/21.
+ */
+$(document).ready(function () {
+    page = new Vue({
+        el: '#pagination',
+        data: {
+            currentPageNo: 1,
+            defaultPageSize: 20,
+            itemCount: 1601,
+            currentResultLength: 20,
+            jumpPageNo: 1,
+            searchInfo: {
+                title: ""
+            }
+        },
+        computed: {
+            pageCount: function () {
+                return Math.ceil(this.itemCount / this.defaultPageSize);
+            },
+            pageList: function () {
+                if (this.itemCount === 0) {
+                    return [];
+                } else {
+                    let min = Math.max(1, this.currentPageNo - 2);
+                    let max = Math.min(this.pageCount, this.currentPageNo + 2);
+                    let arr = [];
+                    for (; min <= max; min++) {
+                        arr.push(min);
+                    }
+                    return arr;
+                }
+            },
+            startIndex: function () {
+                return this.defaultPageSize * (this.currentPageNo - 1) + 1;
+            },
+            endIndex: function () {
+                return this.defaultPageSize * (this.currentPageNo - 1) + this.currentResultLength;
+            }
+        },
+        methods: {
+            resetPage: function (total, resultLength, pageNo, pageSize) {
+                if (typeof(pageSize) !== "undefined") {
+                    this.defaultPageSize = pageSize;
+                }
+                if (typeof(pageNo) !== "undefined") {
+                    this.currentPageNo = pageNo;
+                } else {
+                    if (total !== 0) {
+                        this.currentPageNo = 1;
+                    } else {
+                        this.currentPageNo = 0;
+                    }
+                }
+                this.currentResultLength = resultLength;
+                this.itemCount = total;
+            },
+            jumpPage: function (pageNo) {
+                getList(pageNo);
+            },
+            submit: function () {
+                if (typeof(this.jumpPageNo) === "number") {
+                    this.jumpPage(this.jumpPageNo);
+                }
+            }
+        }
+    });
+    const today = new moment();
+    today.millisecond(0);
+    today.second(0);
+    today.minute(0);
+    today.hour(0);
+    dayUnix = today.valueOf();
+    yearUnix = (new moment(String(today.year()), 'YYYY')).valueOf();
+    let listVue = new Vue({
+        el: '.commodity-list',
+        data: {
+            items: []
+        },
+        filters: {
+            timeHandler: function (value) {
+                if (value > dayUnix) {
+                    return (new moment(value)).format('HH:mm');
+                } else if (value > yearUnix) {
+                    return (new moment(value)).format('MM-DD HH:mm');
+                } else {
+                    return (new moment(value)).format('YYYY-MM-DD HH:mm');
+                }
+            }
+        }
+    });
+
+    function getList(pageNo) {
+        $.get(location.origin + "/query-list",
+            {sort: "id", order: "desc", offset: (pageNo - 1) * page.defaultPageSize, limit: page.defaultPageSize, search: page.searchInfo.title},
+            function (data) {
+                listVue.items = data.rows;
+                page.resetPage(data.total, data.rows.length, pageNo);
+            },
+            "json"
+        );
+    }
+
+    $("#search-btn").click(function () {
+        page.searchInfo.title = $("#search-title").val().trim();
+        getList(1);
+    });
+    getList(1);
+});
