fe163a99bd0ce8070544b261d4e3734c3d2c3cfc
changdy <117969632@qq.com>
2017-06-17 13:03:50 +0800

    增强显示效果

--- smzdm-spider/src/main/java/com/smzdm/service/EmailHandler.java
+++ smzdm-spider/src/main/java/com/smzdm/service/EmailHandler.java
@@ -1,5 +1,6 @@
 package com.smzdm.service;
 
+import com.smzdm.model.Commodity;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Session;
@@ -7,6 +8,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -14,13 +16,14 @@ import java.util.Properties;
  */
 //http://blog.csdn.net/xietansheng/article/details/51673073
 @Service
-public class EmailService {
+public class EmailHandler {
 
     private static final String EMAILACCOUNT = "117969632@qq.com";
     private static final String PASSWORD = "";
     private static final String SMTPHOST = "smtp.qq.com";
     private static final String RECEIVEMAILACCOUNT = "changdy@163.com";
     private static final String smtpPort = "465";
+
 
     private Properties getProperties() {
         Properties props = new Properties();
@@ -55,4 +58,30 @@ public class EmailService {
         transport.sendMessage(message, message.getAllRecipients());
         transport.close();
     }
+
+    public void sendMsg(List<Commodity> matchList) {
+        if (matchList.size() > 0) {
+            StringBuilder content = new StringBuilder("<div style='max-width:700px;'>");
+            try {
+                for (Commodity commodity : matchList) {
+                    content.append("<a style='color:#696969;text-decoration:none;text-align:center;' href='http://www.smzdm.com/p/").append(commodity.getArticleId()).append("'>");
+                    content.append(commodity.getTitle()).append("</a>");
+                    content.append("<div style='color:#aaa'>").append(commodity.getPriceString()).append("</div>");
+                    content.append("<div style='text-align:center'><img src='").append(commodity.getPicUrl()).append("'></div>");
+                    content.append("<div style='color:#F04848;text-indent:10px'>").append(commodity.getContent()).append("</div>");
+                    content.append("<hr/>");
+                }
+                content.append("</div>");
+                System.out.println(content);
+                if (matchList.size() == 1) {
+                    sendMsg(matchList.get(0).getTitle(), content.toString());
+                } else {
+                    sendMsg("捕获到" + matchList.size() + "条优惠信息", content.toString());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
