525730954b7ca902ee159541971c65829e10ee2d
changdy <117969632@qq.com>
2017-07-08 20:12:29 +0800

    增加socket支持

---
+++ smzdm-spider/src/main/java/com/smzdm/service/socket/ServerThread.java
@@ -0,0 +1,43 @@
+package com.smzdm.service.socket;
+
+import com.smzdm.service.CommodityFilterHandler;
+
+import java.io.*;
+import java.net.Socket;
+
+/*
+ * 服务器线程处理类
+ */
+public class ServerThread extends Thread {
+    // 和本线程相关的Socket
+    private Socket socket;
+    private CommodityFilterHandler commodityFilterHandler;
+
+    public ServerThread(Socket socket, CommodityFilterHandler commodityFilterHandler) {
+        this.socket = socket;
+        this.commodityFilterHandler = commodityFilterHandler;
+    }
+
+    //线程执行的操作，响应客户端的请求
+    public void run() {
+        try (InputStream is = socket.getInputStream();
+             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+             BufferedReader br = new BufferedReader(isr);
+             OutputStream os = socket.getOutputStream();
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"))) {
+            //获取输入流，并读取客户端信息
+            StringBuilder info = new StringBuilder();
+            String temp;
+            while ((temp = br.readLine()) != null) {//循环读取客户端的信息
+                info.append(temp);
+            }
+            System.out.println(info);
+            socket.shutdownInput();//关闭输入流
+            //获取输出流，响应客户端的请求
+            bufferedWriter.write("欢迎您！");
+            bufferedWriter.flush();//调用flush()方法将缓冲输出
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ smzdm-spider/src/main/java/com/smzdm/service/socket/SocketServer.java
@@ -0,0 +1,49 @@
+package com.smzdm.service.socket;
+
+import com.smzdm.service.CommodityFilterHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by Changdy on 2017/7/8.
+ */
+@Service
+public class SocketServer {
+
+    private static Logger logger = LoggerFactory.getLogger(SocketServer.class);
+    private CommodityFilterHandler commodityFilterHandler;
+
+    @Autowired
+    public SocketServer(CommodityFilterHandler commodityFilterHandler) {
+        initSocket();
+        this.commodityFilterHandler = commodityFilterHandler;
+    }
+
+    private void initSocket() {
+        try {
+            //1.创建一个服务器端Socket，即ServerSocket，指定绑定的端口，并监听此端口
+            ServerSocket serverSocket = new ServerSocket(8888);
+            //记录客户端的数量
+            //循环监听等待客户端的连接
+            while (true) {
+                //调用accept()方法开始监听，等待客户端的连接
+                Socket socket = serverSocket.accept();
+                //创建一个新的线程
+                ServerThread serverThread = new ServerThread(socket, commodityFilterHandler);
+                //启动线程
+                serverThread.start();
+                InetAddress address = socket.getInetAddress();
+                logger.info("当前客户端的IP：" + address.getHostAddress());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
