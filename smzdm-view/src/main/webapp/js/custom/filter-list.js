15a25356c14c6e25655630462b4e40038e95d1fe
changdy <117969632@qq.com>
2017-07-11 08:49:06 +0800

    增加删除功能

--- smzdm-view/src/main/webapp/js/custom/filter-list.js
+++ smzdm-view/src/main/webapp/js/custom/filter-list.js
@@ -7,9 +7,8 @@ $(document).ready(function () {
         url: reqBashPath + 'query-filter',
         dataType: "json",
         striped: true,                      //是否显示行间隔色
-        singleSelect: true,
         search: true,
-        detailView: true,//显示细节
+        checkbox: true,
         pagination: true, //分页
         pageNumber: 1,                       //初始化加载第一页，默认第一页
         pageSize: 25,                       //每页的记录行数（*）
@@ -17,8 +16,12 @@ $(document).ready(function () {
         showColumns: true,
         sortName: 'id',
         sortOrder: 'desc',
+        toolbar: '#toolbar',
         sidePagination: "server", //服务端处理分页
         columns: [{
+            field: 'state',
+            checkbox: true,
+        }, {
             field: 'id',
             align: 'center',
             valign: 'middle',
@@ -62,4 +65,24 @@ $(document).ready(function () {
             title: '点赞百分比',
         }]
     });
+    $remove = $('#remove');
+    $table.on('check.bs.table uncheck.bs.table ' + 'check-all.bs.table uncheck-all.bs.table', function () {
+        $remove.prop('disabled', !$table.bootstrapTable('getSelections').length);
+        // save your data, here just save the current page
+        selections = getIdSelections();
+        // push or splice the selections if you want to save all data selections
+    });
+    $remove.click(function () {
+        let ids = getIdSelections();
+        $table.bootstrapTable('remove', {
+            field: 'id',
+            values: ids
+        });
+        $remove.prop('disabled', true);
+    });
+    function getIdSelections() {
+        return $.map($table.bootstrapTable('getSelections'), function (row) {
+            return row.id
+        });
+    }
 });
